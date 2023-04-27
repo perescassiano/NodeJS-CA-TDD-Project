10881ecb5280435a0c06cb30d715ed411db0662e
perescassiano <cassianoperes89@gmail.com>
2023-04-27 10:54:57 -0300

    Ignore main in test coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
-  collectCoverageFrom: ['**/src/**/*.js'],
+  collectCoverageFrom: ['**/src/**/*.js', '!**/src/main/*'],
   preset: '@shelf/jest-mongodb'
 }
